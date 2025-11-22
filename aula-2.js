52a3dd006623401d82c96e25d88fde7e3f4d6be3
andersonlietz <andersonrenatolietztj@gmail.com>
2025-11-22 10:42:37 -0300

    Comentando cada componente e adicionando condições no input

--- aula-2.js
+++ aula-2.js
@@ -1,19 +1,40 @@
  function adicionarTarefa() {
-      let mensagem = "Tarefa adicionada com sucesso!";
-         
-      
+    
+      //varre o documento em busca do id inputTarefa
     let inputTarefa = document.getElementById("inputTarefa")
-    let tarefa = inputTarefa.value
-       document.getElementById("mensagem").textContent = tarefa;
+   
 
+    //variável tarefa recebe o valor contido no inputTarefa
+     let tarefa = inputTarefa.value.trim()
+
+     if (tarefa == ""){
+      //define a variavel mensagemErro
+      let mensagemErro = "Adicione uma atividade válida!"  
+
+      //varre o documento em busca do id mensagem e aplica o conteúdo da variável mensagemErro
+      document.getElementById("mensagem").textContent = mensagemErro;
+
+
+     }else{
+     //declaraçã0 da variável mensagem
+      let mensagemSucesso = "Tarefa adicionada com sucesso!";
+  
+    //varre o documento em busca do id mensagem e aplica o conteúdo da variável mensagem
+       document.getElementById("mensagem").textContent = mensagemSucesso;
+
+       //variável listaTarefas varre o documento a procura do elemento de id listaTarefas
        let listaTarefas = document.getElementById("listaTarefas")
+
+       //variável nova tarrefa cria o elemento li
        let novaTarefa =  document.createElement("li")
+
+       //variavel novaTarefa aplica o conteúdo da variável tarefa entre o elemento li
         novaTarefa.textContent = tarefa
 
-
-
+        //o elemento nomeado como listaTarefas recebe um elemento filho chamado novaTarefa
         listaTarefas.appendChild(novaTarefa)
-
-
+     }
+      //esvazia o input após o onClick
        inputTarefa.value = ""
+    
     }
