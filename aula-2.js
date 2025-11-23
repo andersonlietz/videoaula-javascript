53703bd72683ac9bb98d1ef819f903131e7c5a79
andersonlietz <andersonrenatolietztj@gmail.com>
2025-11-23 15:24:38 -0300

    desafio da aula 3 - adicionar cor nas mensagens

--- aula-2.js
+++ aula-2.js
@@ -1,5 +1,9 @@
  function adicionarTarefa() {
     
+
+   // vermelho #A34743 
+   // verde #28A745
+
       //varre o documento em busca do id inputTarefa
     const inputTarefa = document.getElementById("inputTarefa")
    
@@ -7,6 +11,7 @@
     //variável tarefa recebe o valor contido no inputTarefa
      let tarefa = inputTarefa.value.trim()
 
+      //declaração da variável mensagem, que varre o documento e seleciona o que estiver no id mensagem
       const mensagem = document.getElementById("mensagem")
 
      if (tarefa == ""){
@@ -16,13 +21,18 @@
       //varre o documento em busca do id mensagem e aplica o conteúdo da variável mensagemErro
       mensagem.textContent = mensagemErro;
 
+      //define a cor da mensagem de erro
+      mensagem.style.color = "#A34743"
 
      }else{
      //declaraçã0 da variável mensagem
       let mensagemSucesso = "Tarefa adicionada com sucesso!";
   
-    //varre o documento em busca do id mensagem e aplica o conteúdo da variável mensagem
-       mensagem.textContent = mensagemSucesso;
+      //define a cor da mensagem
+      mensagem.style.color = "#28A745"; 
+
+      //varre o documento em busca do id mensagem e aplica o conteúdo da variável mensagem
+      mensagem.textContent = mensagemSucesso;
 
        //variável listaTarefas varre o documento a procura do elemento de id listaTarefas
        const listaTarefas = document.getElementById("listaTarefas")
